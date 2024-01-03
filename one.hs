82e0b4a0e3468137e8a3ae733eabfc6f09af7462
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-01-03 15:57:38 +0100

    Add productList fn

--- one.hs
+++ one.hs
@@ -8,6 +8,10 @@ sumList :: (Num a) => [a] -> a
 sumList [] = 0
 sumList (n : ns) = n + sumList ns
 
+productList :: (Num a) => [a] -> a
+productList [] = 1
+productList (n : ns) = n * productList ns
+
 -- double the value of a given number
 doubleNum :: (Num a) => a -> a
 doubleNum a = 2 * a
@@ -19,8 +23,13 @@ main = do
 
   -- sum of a list of numbers
   let numbers = [1, 2, 3, 4, 5]
-  let result = sumList numbers
-  putStrLn $ "Sum of the list: " ++ show result
+  let sumResult = sumList numbers
+  putStrLn $ "Sum of the list [1, 2, 3, 4, 5]: " ++ show sumResult
+
+  -- product of a list of numbers
+  let otherNumbers = [2, 3, 4]
+  let productResult = productList otherNumbers
+  putStrLn $ "Product of the list [2, 3, 4]: " ++ show productResult
 
   -- double a number
   let x = 16
