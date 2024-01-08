f82de18341f194ccb5f48ebd5a136fdd4b44ee4a
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-01-08 08:49:33 +0100

    Update

--- four.hs
+++ four.hs
@@ -32,22 +32,121 @@ safeTailPatternMatching :: [a] -> [a]
 safeTailPatternMatching [] = []
 safeTailPatternMatching (_ : xs) = xs
 
-main = do
-    putStrLn $ "Halve: " ++ show (halve [1, 2, 3, 4])
+-- disjunctor listing all of the cases explicitely
+disjunctor1 :: Bool -> Bool -> Bool
+disjunctor1 False False = False
+disjunctor1 False True = True
+disjunctor1 True False = True
+disjunctor1 True True = True
 
+-- disjunctor using only two cases
+disjunctor2 :: Bool -> Bool -> Bool
+disjunctor2 False False = False
+disjunctor2 _ _ = True
+
+-- another disjunctor using three cases
+disjunctor3 :: Bool -> Bool -> Bool
+disjunctor3 False b = b
+disjunctor3 b False = b
+disjunctor3 True True = True
+
+-- another disjunctor using three cases
+disjunctor4 :: Bool -> Bool -> Bool
+disjunctor4 True _ = True
+disjunctor4 _ True = True
+disjunctor4 _ _ = False
+
+-- conjunction without using only conditional expressions
+conjunction1 :: Bool -> Bool -> Bool
+conjunction1 b c = if b then if c then True else False else False
+
+-- conjunction without using only conditional expressions
+conjunction2 :: Bool -> Bool -> Bool
+conjunction2 b c = if b then c else False
+
+-- multiply using lambdas
+mult :: Int -> Int -> Int -> Int
+mult = \x -> (\y -> (\z -> x * y * z))
+
+-- doubles a number and subtracts 9 if it is greater than 9
+luhnDouble :: Int -> Int
+luhnDouble x = if t > 9 then t - 9 else t
+  where
+    t = x * 2
+
+-- luhn algorithm for four digits
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn a b c d = (x + b + y + d) `mod` 10 == 0
+  where
+    x = luhnDouble a
+    y = luhnDouble c
+
+main = do
+    -- halve a list into two parts
+    putStrLn $ "Halve of [1, 2, 3, 4]: " ++ show (halve [1, 2, 3, 4])
+
+    -- third element of list
     let numbers = [2, 4, 8, 16, 32]
     putStrLn $ "Third entry of " ++ show numbers ++ ": "
-    print (thirdHeadAndTail numbers)
-    print (thirdListIndexing numbers)
-    print (thirdPatternMatching numbers)
+    putStrLn $ "a) " ++ show (thirdHeadAndTail numbers)
+    putStrLn $ "b) " ++ show (thirdListIndexing numbers)
+    putStrLn $ "c) " ++ show (thirdPatternMatching numbers)
 
-    putStrLn $ "SafeTail of " ++ show numbers ++ "; "
-    print (safeTailConditionalExpr numbers)
-    print (safeTailGuardedEquation numbers)
-    print (safeTailPatternMatching numbers)
+    -- safetail
+    putStrLn $ "SafeTail of " ++ show numbers ++ ": "
+    putStrLn $ "a) " ++ show (safeTailConditionalExpr numbers)
+    putStrLn $ "b) " ++ show (safeTailGuardedEquation numbers)
+    putStrLn $ "c) " ++ show (safeTailPatternMatching numbers)
 
     let emptyList :: [Int] = []
-    putStrLn $ "SafeTail of " ++ show emptyList ++ "; "
-    print (safeTailConditionalExpr emptyList)
-    print (safeTailGuardedEquation emptyList)
-    print (safeTailPatternMatching emptyList)
+    putStrLn $ "SafeTail of " ++ show emptyList ++ ": "
+    putStrLn $ "a) " ++ show (safeTailConditionalExpr emptyList)
+    putStrLn $ "b) " ++ show (safeTailGuardedEquation emptyList)
+    putStrLn $ "c) " ++ show (safeTailPatternMatching emptyList)
+
+    -- disjunction
+    putStrLn "Disjunctor 1:"
+    putStrLn $ "False || False: " ++ show (disjunctor1 False False)
+    putStrLn $ "False || True : " ++ show (disjunctor1 False True)
+    putStrLn $ "True  || False: " ++ show (disjunctor1 True False)
+    putStrLn $ "True  || True : " ++ show (disjunctor1 True True)
+
+    putStrLn "Disjunctor 2:"
+    putStrLn $ "False || False: " ++ show (disjunctor2 False False)
+    putStrLn $ "False || True : " ++ show (disjunctor2 False True)
+    putStrLn $ "True  || False: " ++ show (disjunctor2 True False)
+    putStrLn $ "True  || True : " ++ show (disjunctor2 True True)
+
+    putStrLn "Disjunctor 3:"
+    putStrLn $ "False || False: " ++ show (disjunctor3 False False)
+    putStrLn $ "False || True : " ++ show (disjunctor3 False True)
+    putStrLn $ "True  || False: " ++ show (disjunctor3 True False)
+    putStrLn $ "True  || True : " ++ show (disjunctor3 True True)
+
+    putStrLn "Disjunctor 4:"
+    putStrLn $ "False || False: " ++ show (disjunctor4 False False)
+    putStrLn $ "False || True : " ++ show (disjunctor4 False True)
+    putStrLn $ "True  || False: " ++ show (disjunctor4 True False)
+    putStrLn $ "True  || True : " ++ show (disjunctor4 True True)
+
+    -- conjunction
+    putStrLn "Conjunction 1:"
+    putStrLn $ "False || False: " ++ show (conjunction1 False False)
+    putStrLn $ "False || True : " ++ show (conjunction1 False True)
+    putStrLn $ "True  || False: " ++ show (conjunction1 True False)
+    putStrLn $ "True  || True : " ++ show (conjunction1 True True)
+
+    putStrLn "Conjunction 2:"
+    putStrLn $ "False || False: " ++ show (conjunction2 False False)
+    putStrLn $ "False || True : " ++ show (conjunction2 False True)
+    putStrLn $ "True  || False: " ++ show (conjunction2 True False)
+    putStrLn $ "True  || True : " ++ show (conjunction2 True True)
+
+    -- multiplication
+    putStrLn $ "Mult(2, 3, 4): " ++ show (mult 2 3 4)
+
+    -- luhn algorithm
+    putStrLn $ "luhnDouble 3: " ++ show (luhnDouble 3)
+    putStrLn $ "luhnDouble 6: " ++ show (luhnDouble 6)
+    putStrLn $ "luhn(1, 7, 8, 4): " ++ show (luhn 1 7 8 4)
+    putStrLn $ "luhn(4, 7, 8, 3): " ++ show (luhn 4 7 8 3)
--- three.hs
+++ three.hs
@@ -1,33 +1,44 @@
+-- returns a list of bools
 bools :: [Bool]
 bools = [True, True, False]
 
+-- returns a list of list of numbers
 nums :: [[Int]]
 nums = [[0], [1, 2], [1, 2, 3]]
 
+-- adds three arguments
 add :: Int -> Int -> Int -> Int
 add x y z = x + y + z
 
+-- doubles a given argument
 copy :: a -> (a, a)
 copy a = (a, a)
 
+-- applies a given function to an argument
 apply :: (a -> b) -> a -> b
 apply f a = f a
 
+-- returns the second element
 second :: [x] -> x
 second xs = head (tail xs)
 
+-- swaps two element in a two tuple
 swap :: (x, y) -> (y, x)
 swap (x, y) = (y, x)
 
+-- creates a pair from two values
 pair :: x -> y -> (x, y)
 pair x y = (x, y)
 
+-- doubles a given number
 double :: (Num x) => x -> x
 double x = x * 2
 
+-- returns whether a list is a palindrome
 palindrome :: (Eq x) => [x] -> Bool
 palindrome xs = reverse xs == xs
 
+-- applies a given function twice
 twice :: (x -> x) -> x -> x
 twice f x = f (f x)
 
